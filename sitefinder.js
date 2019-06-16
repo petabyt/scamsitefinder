889d87796e4dcb76ff3d7c937d8b6f89e31f0327
Pufflegamerz <myneweb14>
2019-06-16 17:51:58 -0500

    Upload
    Cool thingies

--- sitefinder.js
+++ sitefinder.js
@@ -21,70 +21,32 @@ var names = [
 var sites = [
 	
 ];
-var stopSearching = false;
-
-var timeouts = 0;
-var successes = 0;
+var interval;
 
 window.onload = function() {
-	console.log("*Errors are normal.*")
+
 }
 
 function start() {
-	createIframe();
-}
-
-function createIframe() {
-	if (!stopSearching) {
+	interval = setInterval(function() {
 		var rand = Math.floor(Math.random() * names.length);
-		document.getElementById('main').innerHTML = "<iframe id='frame' onload='deleteIframe(\"success\")' src='" + ("https://" + misspell(names[rand]) + ".com") + "'></iframe>";
-
-		// Load Timeout
-		var si1 = setInterval(function() {
-			deleteIframe("timeout");
-		}, 5000)
-	}
-}
-
-function deleteIframe(result) {
-	if (!stopSearching) {
-
-		// If iframe glitches out, do nothing about it
-		if (!(!(document.getElementById('frame')))) {
-			var site = document.getElementById('frame').src;
-			if (result == "timeout") {
-				timeouts++
-			} else {
-				sites.push("<a href='" + site + "'>" + site + "</a>")
-				successes++
-			}
-			document.getElementById('main').innerHTML = "";
-		}
-
-		// So it doesn't crash
-		var si2 = setInterval(function() {
-			createIframe();
-		}, 2000);
+		sites.push("<a href='https://" + misspell(names[rand]) + ".com'>https://" + misspell(names[rand]) + ".com</a>");
 
 		update();
-	}
+	}, 1);
 }
 
+
 function update() {
-	document.getElementById('siteList').innerHTML = sites.join("<br>");
-	document.getElementById('stats').innerHTML = "Tested " + (successes + timeouts) + " sites. " + successes + " loaded correctly. " + timeouts + " didn't load." + Math.round(successes/(successes + timeouts) * 100) + "% of sites tested were successfully."
-	if (successes + timeouts > 100) {
-		alert("Process automatically stopped to prevent crashing.");
-		stop();
+	document.getElementById('siteList').innerHTML += sites[sites.length - 1] + "<br>";
+	document.getElementById('stats').innerHTML = "Generated " + sites.length + " websites";
+
+	if (sites.length > 50) {
+		document.getElementById('siteList').children[0].outerHTML = "";
+		document.getElementById('siteList').children[1].outerHTML = "";
 	}
 }
 
 function stop() {
-	if (stopSearching == false) {
-		stopSearching = true;
-	} else {
-		stopSearching = false;
-	}
-	clearInterval(si1);
-	clearInterval(si2);
+	clearInterval(interval);
 }
